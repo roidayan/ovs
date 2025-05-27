roidayan/ovs

commit ee89ea7b477bb4fd05137de03b2e8443807ed9f4

json: Move from lib to include/openvswitch.
To easily allow both in- and out-of-tree building of the Python
wrapper for the OVS JSON parser (e.g. w/ pip), move json.h to
include/openvswitch. This also requires moving lib/{hmap,shash}.h.

Both hmap.h and shash.h were #include-ing "util.h" even though the
headers themselves did not use anything from there, but rather from
include/openvswitch/util.h. Fixing that required including util.h
in several C files mostly due to OVS_NOT_REACHED and things like
xmalloc.

Signed-off-by: Terry Wilson <twilson@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

lib/util.h
@@ -25,6 +25,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "compiler.h"
+#include "util.h"
 #include "openvswitch/util.h"
 
 extern char *program_name;

commit f7de082772887d72803fd01b3a0b0969190cad3a

util: Remove include of itself.
This is a redundant include.

Fixes: ee89ea7b477b ("json: Move from lib to include/openvswitch.")
Signed-off-by: Roi Dayan <roid@nvidia.com>
Change-Id: I6b43940dc4f64640520cbde94d86e43eeebc8649

lib/util.h
@@ -27,7 +27,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "compiler.h"
-#include "util.h"
 #include "openvswitch/util.h"
 #if defined(__aarch64__) && __GNUC__ >= 6
 #include <arm_neon.h>